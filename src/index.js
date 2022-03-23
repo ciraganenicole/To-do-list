bbb39d9c5782eb4245580b00f73dfdeda7b94c66
Ciragane Nicole <ciraganenicole@gmail.com>
2022-03-23 11:08:35 +0100

    Create source files

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import './style.css';
+
+class Tasks {
+  constructor() {
+    this.tasks = [];
+  }
+
+    generate = () => {
+      const dell = this.tasks.map((task) => ` <li class="task">
+               <div> <button type="button"><i class="fa-regular fa-square" ${task.completed} ></i></button>
+                <p class="text">${task.description}</p></div>
+                <a href=""><i class="fa-solid fa-ellipsis-vertical" ${task.index += 1}></i></a>
+                </li>`).join(' ');
+      const container = document.getElementById('tasks');
+      container.innerHTML = dell;
+    }
+
+    newTask = (value) => {
+      this.tasks.push({ description: value, completed: false, index: 0 });
+      this.generate();
+    }
+}
+
+const taskList = new Tasks();
+taskList.generate();
+
+const addTask = () => {
+  const input = document.getElementById('add');
+  input.addEventListener('change', (e) => {
+    e.preventDefault();
+    taskList.newTask(input.value);
+    input.value = '';
+  });
+};
+
+addTask();
