779e7ebfb1d980c6225e5d435bc8b89e96cd4b55
Ciragane Nicole <ciraganenicole@gmail.com>
2022-03-26 14:47:32 +0100

    Implement the clear all checked feature

---
+++ modules/Tasks.js
@@ -0,0 +1,24 @@
+class Task {
+  constructor(description, completed, index, editable) {
+    this.description = description;
+    this.completed = completed || false;
+    this.index = index;
+    this.editable = editable || false;
+  }
+
+  getHtml() {
+    return ` <li class="task" draggable="true">
+           <div class="description">
+        <input type="checkbox" class="check"  id="checkbox-${this.index - 1}" ${this.completed ? 'checked' : ''}/>
+        ${!this.editable ? `<p class="text">${this.description}</p>` : ''}
+        ${this.editable ? `<input value='${this.description}' id="input-${this.index - 1}"/>` : ''}</div>
+     <div id="myLinks-${this.index - 1}" style="display:none" class="list" >
+     <a href="#" class='del' id="del-${this.index - 1}">Delete</a>
+     <a href="#" class='edit'>Edit</a>
+   </div>
+     <button class="remove" id="remove-${this.index - 1}"><i class="fa-solid fa-ellipsis-vertical ellips"></i></button>
+     </li>`;
+  }
+}
+
+export default Task;
---
+++ modules/TasksList.js
@@ -0,0 +1,102 @@
+import hamburger from './hamburger.js';
+import Task from './Tasks.js';
+
+class Tasks {
+  constructor() {
+    this.tasks = [];
+  }
+
+  initialize() {
+    const stored = localStorage.getItem('taskData');
+    if (stored) {
+      this.tasks = JSON.parse(stored)
+        .map((task, index) => new Task(task.description, task.completed, index + 1));
+      this.setHtml();
+    }
+    this.setup();
+  }
+
+  setup() {
+    this.setupRemove();
+    this.setupComplete();
+    this.setupClearAll();
+  }
+
+  setupClearAll() {
+    document.getElementById('clear').addEventListener('click', () => {
+      this.assignTasks(this.tasks.filter((task) => !task.completed));
+    });
+  }
+
+  assignTasks(tasks = this.tasks) {
+    const newTasks = [];
+    tasks.forEach((t, index) => {
+      newTasks.push(new Task(t.description, t.completed, index + 1, t.editable));
+    });
+    this.tasks = newTasks;
+    localStorage.setItem('taskData', JSON.stringify(this.tasks.map((t, index) => ({ ...t, index }))));
+    this.setHtml();
+    this.setup();
+  }
+
+  setupRemove() {
+    hamburger();
+    const del = document.querySelectorAll('.del');
+    del.forEach((button) => {
+      const removeFunc = () => {
+        const index = parseInt(button.id.replace('del-', ''), 10);
+        const newTasks = this.tasks.filter((t) => t.index !== index + 1);
+        this.assignTasks(newTasks);
+      };
+      button.onclick = removeFunc;
+    });
+
+    const edit = document.querySelectorAll('.edit');
+    edit.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        this.tasks[index].editable = true;
+        this.assignTasks();
+        const input = document.getElementById(`input-${index}`);
+        input.addEventListener('change', (e) => {
+          e.preventDefault();
+          this.tasks[index].description = e.target.value;
+          this.tasks[index].editable = false;
+          this.assignTasks();
+        });
+      });
+    });
+  }
+
+  setupComplete() {
+    const checkboxList = document.querySelectorAll('.check');
+    checkboxList.forEach((checkbox) => {
+      checkbox.addEventListener('click', () => {
+        this.tasks[parseInt(checkbox.id.replace('checkbox-', ''), 10)].completed = checkbox.checked;
+        this.assignTasks();
+      });
+    });
+  }
+
+  newTask(description, completed) {
+    const task = new Task(description, completed, this.tasks.length + 1);
+    this.tasks.push(task);
+    this.assignTasks();
+    return task;
+  }
+
+  getBookList() {
+    let containerHtml = '';
+    this.tasks.forEach((task) => {
+      containerHtml += task.getHtml();
+    });
+    return containerHtml;
+  }
+
+  setHtml() {
+    const container = document.getElementById('tasks');
+    container.innerHTML = this.getBookList();
+    this.setupRemove();
+  }
+}
+
+export default Tasks;
